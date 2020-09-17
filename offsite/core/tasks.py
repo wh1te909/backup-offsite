a5e82671bc89830c1b9e4b287c7e1951b0420ef2
wh1te909 <dcparsi@gmail.com>
2020-09-17 03:58:27 +0000

    fix cli args for new go agent

--- offsite/core/tasks.py
+++ offsite/core/tasks.py
@@ -241,7 +241,7 @@ def auto_offsite_task():
         rand_name = "".join(random.choice(string.ascii_letters) for _ in range(20))
         log_name = f"/offsitelogs/{rand_name}.log"
 
-        cmd = f"tacticaloffsite --source={source} --dest={agent.path} --limit={agent.rsync_bwlimit} --logfile={log_name}"
+        cmd = f"tacticaloffsite -source {source} -dest {agent.path} -limit {agent.rsync_bwlimit} -logfile {log_name}"
 
         r = agent.salt_api_cmd(
             timeout=10,
